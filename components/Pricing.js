519c8059480724e5379c582bcfb0d401dd6af2b7
NaseerFast <jaafar.nasir71@gmail.com>
2023-03-13 13:51:56 +0100

    up

--- components/Pricing.js
+++ components/Pricing.js
@@ -216,34 +216,24 @@ const Pricing = () => {
           </div>
         </div>
         <div className="flex flex-col w-full my-16">
-          <ScrollAnimationWrapper>
-            <motion.h3
-              variants={scrollAnimation}
-              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed w-9/12 sm:w-6/12 lg:w-4/12 mx-auto">
-             Pickup Hours{" "}
-            </motion.h3>
-            {/* <motion.p className="leading-normal  mx-auto my-2 w-10/12 sm:w-7/12 lg:w-6/12" variants={scrollAnimation}>
-              See LaslesVPN everywhere to make it easier for you when you move
-              locations.
-            </motion.p> */}
-          </ScrollAnimationWrapper>
-          <ScrollAnimationWrapper>
-            <motion.div className="py-1 w-full px-8 mt-1" variants={scrollAnimation}> 
-              {/* <Maps className="w-full h-auto" /> */}
-              <div className="space-y-4">
-                
-                <p>No 6, Kontagora Street</p>
-                <p>GRA POLO, Maiduguri Borno State</p>
-                <p>Saturday - Thursday 9am - 4pm</p>
-              
-                <ButtonOutline>Get Directions</ButtonOutline>
+         
+{/* 
+<ScrollAnimationWrapper className="relative w-full mt-16">
+            <motion.div variants={scrollAnimation} custom={{duration: 3}}>
+              <div className="absolute rounded-xl  py-8 sm:py-14 px-6 sm:px-12 lg:px-16 w-full flex flex-col   items-center z-10 bg-white-500">
+              <p>No 6, Kontagora Street</p>
+               
+                <ButtonPrimary>Get Started</ButtonPrimary>
               </div>
-            
+              <div
+                className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-60 sm:h-56 top-0 mt-8 mx-auto left-0 right-0"
+                style={{ filter: "blur(114px)" }}
+                ></div>
             </motion.div>
-          </ScrollAnimationWrapper>
-          <ScrollAnimationWrapper>
+          </ScrollAnimationWrapper> */}
+          {/* <ScrollAnimationWrapper>
             <motion.div className="w-full flex justify-evenly items-center mt-4 flex-wrap lg:flex-nowrap" variants={scrollAnimation}>
-              {/* <Netflix className="h-18 w-auto" /> */}
+             
 
              
               <img
@@ -272,7 +262,7 @@ const Pricing = () => {
                 alt=""
               />
             </motion.div>
-          </ScrollAnimationWrapper>
+          </ScrollAnimationWrapper> */}
         </div>
         <div className="flex flex-col w-full my-16" id="testimoni">
           <ScrollAnimationWrapper>
@@ -296,14 +286,17 @@ const Pricing = () => {
           </ScrollAnimationWrapper>
           <ScrollAnimationWrapper className="relative w-full mt-16">
             <motion.div variants={scrollAnimation} custom={{duration: 3}}>
-              <div className="absolute rounded-xl  py-8 sm:py-14 px-6 sm:px-12 lg:px-16 w-full flex flex-col sm:flex-row justify-between items-center z-10 bg-white-500">
-                <div className="flex flex-col text-left w-10/12 sm:w-7/12 lg:w-5/12 mb-6 sm:mb-0">
-                  <h5 className="text-black-600 text-xl sm:text-2xl lg:text-3xl leading-relaxed font-medium">
-                    Subscribe Now for <br /> Get Special Features!
+              <div className="absolute rounded-xl  py-8 sm:py-14 md:py-6 px-6 sm:px-12 lg:px-16 w-full flex flex-col  text-center items-center z-10 bg-white-500">
+                <div className="flex flex-col  w-10/12 sm:w-7/12 lg:w-5/12 mb-6 sm:mb-0 space-y-4">
+                  <h5 className="text-black-600 text-lg sm:text-xl lg:text-2xl leading-relaxed font-medium">
+                 Pick Up Location
                   </h5>
-                  <p>Let's subscribe with us and find the fun.</p>
+                  <p className="py-4">No 6 Kontagora Street <br />
+                  GRA POLO, Maiduguri <br />
+                9AM - 4PM
+                  </p>
                 </div>
-                <ButtonPrimary>Get Started</ButtonPrimary>
+                <ButtonPrimary>Get Directions</ButtonPrimary>
               </div>
               <div
                 className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-60 sm:h-56 top-0 mt-8 mx-auto left-0 right-0"
